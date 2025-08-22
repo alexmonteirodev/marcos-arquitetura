f52c714c12a007cbb288a9dd614531aa5307c31e
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-08-22 17:47:53 +0200

    menumobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -50,17 +50,30 @@ const Header = () => {
         <div></div>
       </button>
       {mobileMenu && (
-        <div className="fixed inset-0 z-50 flex flex-col items-center justify-center backdrop-blur-md gap-8">
-          {links.map((link, i) => (
-            <Link
-              key={i}
-              href={link.href}
-              onClick={() => setMobileMenu(false)}
-              className="px-8 py-3 rounded-full text-base-400 text-sm bg-base-200 transition-all duration-200 ease-in-out hover:bg-base-400 hover:text-base-100"
-            >
-              {link.label}
-            </Link>
-          ))}
+        <div className="fixed inset-0 z-50 flex flex-col items-center justify-center backdrop-blur-xl gap-8">
+          <button
+            onClick={() => setMobileMenu(false)}
+            className="size-12 text-2xl font-bold absolute top-8 right-8 bg-base-100 p-7 rounded-full flex justify-center items-center"
+          >
+            <p>x</p>
+          </button>
+          {links.map((link, i) => {
+            const isActive = pathName === link.href;
+            return (
+              <Link
+                key={i}
+                href={link.href}
+                onClick={() => setMobileMenu(false)}
+                className={`px-8 py-3 font-bold text-xl  animate-slideIn transition-all duration-200 ease-in-out ${
+                  isActive
+                    ? "bg-base-400 text-base-100 rounded-xl"
+                    : "bg-base-200 text-base-400 rounded-full"
+                }`}
+              >
+                {link.label}
+              </Link>
+            );
+          })}
         </div>
       )}
     </header>
